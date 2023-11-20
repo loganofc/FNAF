3cea39cddcda77ec63064f83517a0d22512aa445
logan <141536342+loganofc@users.noreply.github.com>
2023-11-20 12:10:37 -0600

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,8 @@
+function getMessage() {
+  return "Welcome to FNAF, Play For free and as long as you want! :)";
+}
+(function (_0x39c2f3, _0x6523f) {
+  _0x39c2f3.onload = function () {
+    alert("Welcome to FNAF, Play For free and as long as you want! :)");
+  };
+})(window, document);
